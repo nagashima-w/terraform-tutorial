2431402241d587068e2d0beda939fedd19e74566
Wataru Nagashima <wataru.nagashima@ghelia.com>
2019-10-03 19:02:05 +0900

    add sg

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-
 data "aws_ami" "recent_amazon_linux_2" {
   most_recent = true
   owners      = ["amazon"]
@@ -18,10 +17,28 @@ data "aws_ami" "recent_amazon_linux_2" {
   }
 }
 
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.recent_amazon_linux_2.image_id
-  instance_type = "t3.micro"
-  
+  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
   tags = {
       Name = "example"
   }
@@ -35,4 +52,8 @@ EOF
 
 output "example_instance_id" {
   value = "aws_instance.example.id"
+}
+
+output "example_instance_public_dns" {
+  value = "aws_instance.example.public_dns"
 }
